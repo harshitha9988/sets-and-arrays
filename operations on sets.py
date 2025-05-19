1054683b53aabb89abae2f6763ed0c9d8a9e0238
harshitha9988 <harshitha.uppala@outlook.com>
2025-05-18 20:25:30 -0400

    operations with sets

---
+++ operations on sets.py
@@ -0,0 +1,16 @@
+x={1,2,3}
+print(x)
+
+y={1.0,"Helllo", (1,2,3)}
+print(y)
+
+z=set([1,2,3,2])
+print(z,"\n")
+
+a=set([0,1,3,4,5])
+print("Original set:")
+print(a)
+
+a.pop()
+print("After removing the fist element from the said set")
+print(a,"\n")
